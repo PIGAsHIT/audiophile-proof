053f1d6d6100325598d4fdb043a49caf52e34c74
PIGAsHIT <93385350+PIGAsHIT@users.noreply.github.com>
2026-02-10 20:50:53 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from src.db.postgres import engine, Base
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-    # 🚀 在 CI 測試開始前，自動建立所有資料表
     Base.metadata.create_all(bind=engine)
     yield
     
